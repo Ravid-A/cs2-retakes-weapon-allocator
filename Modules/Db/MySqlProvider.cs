665c33e2065afb2dad2b615028c3d1541a740c24
Ravid-A <ravid2903@gmail.com>
2026-06-05 15:55:47 +0300

    feat(db): add MySQL provider, sqlite config fields, and provider factory

---
+++ Modules/Db/MySqlProvider.cs
@@ -0,0 +1,28 @@
+using System.Data.Common;
+using MySqlConnector;
+
+namespace RetakesAllocator.Modules;
+
+public class MySqlProvider : IDatabaseProvider
+{
+    private readonly string _connectionString;
+
+    public MySqlProvider(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public DbConnection CreateConnection() => new MySqlConnection(_connectionString);
+
+    public string CreateTableSql =>
+        "CREATE TABLE IF NOT EXISTS `weapons` ( " +
+        "`id` INT NOT NULL AUTO_INCREMENT, " +
+        "`auth` VARCHAR(128) NOT NULL, " +
+        "`name` VARCHAR(128) NOT NULL, " +
+        "`t_primary` INT NOT NULL DEFAULT 0, " +
+        "`ct_primary` INT NOT NULL DEFAULT 0, " +
+        "`t_secondary` INT NOT NULL DEFAULT 0, " +
+        "`ct_secondary` INT NOT NULL DEFAULT 0, " +
+        "`give_awp` INT NOT NULL DEFAULT 0, " +
+        "PRIMARY KEY (`id`), UNIQUE (`auth`)) ENGINE = InnoDB;";
+}
