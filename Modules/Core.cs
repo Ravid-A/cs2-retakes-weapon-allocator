5c4f11bf395936b0920b89e73f0a82bd1ad2b018
Ravid-A <ravid2903@gmail.com>
2026-06-05 16:03:31 +0300

    refactor(db): wire Core and Utils to async WeaponStore
    Replace static Database callbacks with WeaponStore async calls.
    Core.InitializeDatabase uses DatabaseProviderFactory; Utils.AddPlayerToList
    and RemovePlayerFromList use Task.Run + Server.NextFrame for threading safety.

--- Modules/Core.cs
+++ Modules/Core.cs
@@ -3,14 +3,11 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Modules.Cvars;
 
-using MySqlConnector;
-
 using RetakesAllocator.Modules.Models;
 using RetakesAllocator.Modules.Weapons;
 using RetakesAllocator.Modules.Votes;
 
 using static RetakesAllocator.Modules.RetakeCapability;
-using static RetakesAllocator.Modules.Database;
 using static RetakesAllocator.Modules.Utils;
 using static RetakesAllocator.Modules.Configs;
 using static RetakesAllocator.Modules.Handlers.Commands;
@@ -34,7 +31,7 @@ public class Core : BasePlugin
     public static Config Config = null!;
     public static NadesConfig NadesConfig = null!;
 
-    public static Database Db = null!;
+    public static WeaponStore Store = null!;
     public static List<Player> Players = new();
     public static int RoundsCounter = 0;
     public static AsyncVoteManager currentVote = null!;
@@ -54,7 +51,7 @@ public class Core : BasePlugin
 
         LoadConfigs();
 
-        Connect(SQL_ConnectCallback);
+        InitializeDatabase();
 
         RegisterCommands();
         RegisterEvents();
@@ -72,7 +69,7 @@ public class Core : BasePlugin
     {
         UnRegisterCommands();
         Votes_OnPluginUnload();
-        Utilities.GetPlayers().ForEach(RemovePlayerFromList);
+        Utilities.GetPlayers().ForEach(p => RemovePlayerFromList(p, flush: true));
 
         RetakeCapability_OnUnload();
     }
@@ -91,53 +88,18 @@ public class Core : BasePlugin
         return gameRules;
     }
 
-    private static void SQL_ConnectCallback(string connectionString, Exception exception, dynamic data)
+    private static void InitializeDatabase()
     {
-        if (connectionString == null!)
+        try
         {
-            ThrowError($"Failed to connect to database: {exception.Message}");
-            return;
+            var provider = DatabaseProviderFactory.Create(Config.DbConnection, Plugin.ModuleDirectory);
+            Store = new WeaponStore(provider);
+            Store.InitializeAsync().GetAwaiter().GetResult();
+            PrintToServer("Connected to database");
         }
-
-        Db = new Database(connectionString);
-
-        PrintToServer("Connected to database");
-
-        CreateTables();
-    }
-
-    public static void SQL_FetchUser_CB(MySqlDataReader reader, Exception exception, dynamic data)
-    {
-        if (exception != null!)
+        catch (Exception e)
         {
-            ThrowError($"Database error, {exception.Message}");
-            return;
-        }
-
-        if (reader.HasRows)
-        {
-            while(reader.Read())
-            {
-                var tPrimary = reader.GetInt32("t_primary");
-                var ctPrimary = reader.GetInt32("ct_primary");
-                var tsecondary = reader.GetInt32("t_secondary");
-                var ctSecondary = reader.GetInt32("ct_secondary");
-                var giveAwp = (GiveAwp)reader.GetInt32("give_awp");
-
-                Player player = Players[data];
-
-                player.WeaponsAllocator.PrimaryWeaponT = tPrimary > PrimaryT.Count ? 0 : tPrimary;
-                player.WeaponsAllocator.PrimaryWeaponCt = ctPrimary > PrimaryCt.Count ? 0 : ctPrimary;
-                player.WeaponsAllocator.SecondaryWeaponT = tsecondary > PistolsT.Count ? 0 : tsecondary;
-                player.WeaponsAllocator.SecondaryWeaponCt = ctSecondary > PistolsCT.Count ? 0 : ctSecondary;
-                player.WeaponsAllocator.GiveAwp = giveAwp;
-            }
-        }
-        else
-        {
-            Player player = Players[data];
-
-            Query(SQL_CheckForErrors, $"INSERT INTO `weapons` (`auth`, `name`) VALUES ('{player.GetSteamId2()}', '{EscapeString(player.GetName())}')");
+            ThrowError($"Failed to connect to database: {e.Message}");
         }
     }
 
